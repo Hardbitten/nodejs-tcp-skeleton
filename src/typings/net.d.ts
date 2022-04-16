4bd65aa15b9bab8ce83b28ca6f0ac8ce36621854
Mahdi Sarafraz <hardbitten7697@gmail.com>
2022-04-16 12:01:37 +0430

    Socket rooms Added.

--- src/typings/net.d.ts
+++ src/typings/net.d.ts
@@ -3,15 +3,14 @@ declare module "net" {
   export interface Socket {
     // attributes
     _id: string;
+    rooms: Map<string, string>;
 
     // functions
     joinRoom(room: string, socket_id: string): void;
     leaveRoom(room: string, socket_id: string): void;
-
-    rooms: Map<string, string>;
   }
   export interface Server {
     sockets: Map<string, Socket>;
-    rooms: Map<string, Set<string>>;
+    rooms: Map<string, /* socket list */ Set<string>>;
   }
 }
